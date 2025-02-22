679e906c104dfc6ed3bf4920760f7046189b8fcb
Epicpoop21 <73061192+Epicpoop21@users.noreply.github.com>
2025-02-22 10:59:21 +0000

    Drag&drop attemped

--- Assets/Scripts/UI/DragAndDrop.cs
+++ Assets/Scripts/UI/DragAndDrop.cs
@@ -1,16 +1,25 @@
 using UnityEngine;
+using UnityEngine.EventSystems;
 
-public class DragAndDrop : MonoBehaviour
+public class DragAndDrop : MonoBehaviour, IBeginDragHandler, IDragHandler, IEndDragHandler
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-        
-    }
+	private Vector3 originalPosition;
+	private Transform parentAfterDrag;
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+	public void OnBeginDrag(PointerEventData eventData)
+	{
+		originalPosition = transform.position;
+		parentAfterDrag = transform.parent;
+		transform.SetParent(transform.root); // Move to root to avoid layout issues
+	}
+
+	public void OnDrag(PointerEventData eventData)
+	{
+		transform.position = Input.mousePosition; // Move with cursor
+	}
+
+	public void OnEndDrag(PointerEventData eventData)
+	{
+		transform.SetParent(parentAfterDrag); // Reset parent after drag
+	}
 }
---
+++ Assets/Scripts/UI/DropHandler.cs
@@ -0,0 +1,33 @@
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class DropHandler : MonoBehaviour, IDropHandler
+{
+	private int droppedParts = 0;
+	public GameObject[] newParts; 
+	public Transform partsContainer; 
+
+	public void OnDrop(PointerEventData eventData)
+	{
+		GameObject droppedObject = eventData.pointerDrag;
+		if (droppedObject != null)
+		{
+			Destroy(droppedObject);
+			droppedParts++;
+
+			if (droppedParts == 3) 
+			{
+				Invoke(nameof(SpawnNewParts), 0.5f);
+			}
+		}
+	}
+
+	private void SpawnNewParts()
+	{
+		droppedParts = 0;
+		foreach (GameObject newPart in newParts)
+		{
+			Instantiate(newPart, partsContainer);
+		}
+	}
+}
