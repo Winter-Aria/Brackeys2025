d83efbcc92b3415efdbbbe59e3f6ebd9fd5a1f88
Epicpoop21 <73061192+Epicpoop21@users.noreply.github.com>
2025-02-23 00:36:52 +0000

    Fixed FixEngine quest

--- Assets/Resources/Quests/FixEngine/FixEngine.cs
+++ Assets/Resources/Quests/FixEngine/FixEngine.cs
@@ -1,9 +1,11 @@
+using Mono.Cecil.Cil;
 using UnityEngine;
 
 public class FixEngine : QuestStep
 {
 	private int partsFixed = 0;
 	private int partsToFix = 3;
+	private bool isNearToEngine = false;
 
 	[SerializeField] private GameObject engineFixerUIPanelPrefab;
 	private GameObject engineFixerUIPanel;
@@ -11,17 +13,26 @@ public class FixEngine : QuestStep
 	private void OnEnable()
 	{
 		EventManager.Instance.taskEvents.newPartDroppedIn += NewPartDroppedIn;
+		EventManager.Instance.taskEvents.enterEngineArea += EnterEngineArea;
 	}
 
 	private void OnDisable()
 	{
 		EventManager.Instance.taskEvents.newPartDroppedIn -= NewPartDroppedIn;
+		EventManager.Instance.taskEvents.enterEngineArea += EnterEngineArea;
 	}
 
 	private void Start()
 	{
 		EventManager.Instance.uiEvents.StartNewStep("FixEngine", 3);
-		engineFixerUIPanel = Instantiate(engineFixerUIPanelPrefab, GameObject.Find("Canvas").transform);
+	}
+
+	private void Update()
+	{
+		if (isNearToEngine == true && Input.GetKeyDown(KeyCode.E))
+		{
+			engineFixerUIPanel = Instantiate(engineFixerUIPanelPrefab, GameObject.Find("Canvas").transform);
+		}
 	}
 
 	//Incriment amount of rubbish collected upon collecting. If enough is collected, finish the step
@@ -39,5 +50,17 @@ public class FixEngine : QuestStep
 			EventManager.Instance.questSystemEvents.UpdateProgress("FixEngine", 3);
 			FinishQuestStep();
 		}
+	}
+
+	private void EnterEngineArea(bool enterOrExit)
+	{
+		if (enterOrExit)
+		{
+			isNearToEngine = true;
+		} else if (!enterOrExit)
+		{
+			isNearToEngine = false;
+		}
+		
 	}
 }
